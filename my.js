f319af01a7b522e6060b8260b14ad15b34395091
Ardalan Shahandeh <43750987+ardalan-shahandeh@users.noreply.github.com>
2023-02-05 13:45:20 +0330

    Add files via upload

--- my.js
+++ my.js
@@ -1,54 +1,49 @@
 ///////////////////TODO List///////////////
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var TodoState;
 (function (TodoState) {
     TodoState[TodoState["New"] = 0] = "New";
     TodoState[TodoState["Active"] = 1] = "Active";
     TodoState[TodoState["Compelete"] = 2] = "Compelete";
-    TodoState[TodoState["Delete"] = 3] = "Delete";
+    TodoState[TodoState["Deleted"] = 3] = "Deleted";
 })(TodoState || (TodoState = {}));
-// var todo = {
-//   name: 'Bank',
-//   // state: TodoState.Active
-//   get state() {
-//     return this._state
-//   },
-//   set state(newState) {
-//     if (newState === TodoState.Compelete) 
-//     {
-//       var canBeCompele  =
-//       this.state == TodoState.Active || this.state==TodoState.Delete
-//       if(!canBeCompele) 
-//       {
-//         throw "Todo must be active"
-//       }
-//     }
-//     this._state = newState
-//   }
-// }
-// todo.state=TodoState.New;
-// console.log(todo.state);
-var smartTodo = /** @class */ (function () {
-    function smartTodo(name) {
-        this.name = name;
+var TodoStateChanger = /** @class */ (function () {
+    function TodoStateChanger(newState) {
+        this.newState = newState;
     }
-    Object.defineProperty(smartTodo.prototype, "state", {
-        get: function () {
-            return this._state;
-        },
-        set: function (newState) {
-            if (newState === TodoState.Compelete) {
-                var canBeCompele = this.state == TodoState.Active || this.state == TodoState.Delete;
-                if (!canBeCompele) {
-                    throw "Todo must be active";
-                }
-            }
-            this._state = newState;
-        },
-        enumerable: false,
-        configurable: true
-    });
-    return smartTodo;
+    TodoStateChanger.prototype.canChangeState = function (todo) {
+        return !!todo;
+    };
+    TodoStateChanger.prototype.changeState = function (todo) {
+        if (this.canChangeState(todo)) {
+            todo.state = this.newState;
+        }
+        return todo;
+    };
+    return TodoStateChanger;
 }());
-var todo = new smartTodo("bank");
-todo.state = TodoState.Compelete;
-var st = todo.state;
+var compeleteTodoStateChanger = /** @class */ (function (_super) {
+    __extends(compeleteTodoStateChanger, _super);
+    function compeleteTodoStateChanger() {
+        return _super.call(this, TodoState.Compelete) || this;
+    }
+    compeleteTodoStateChanger.prototype.canChangeState = function (todo) {
+        return _super.prototype.canChangeState.call(this, todo) && (todo.state == TodoState.Active || todo.state == TodoState.Deleted);
+    };
+    return compeleteTodoStateChanger;
+}(TodoStateChanger));
+var st = new compeleteTodoStateChanger();
--- my.ts
+++ my.ts
@@ -9,61 +9,34 @@ enum TodoState {
   New,
   Active,
   Compelete,
-  Delete
+  Deleted
 }
 
-// var todo = {
-//   name: 'Bank',
-//   // state: TodoState.Active
-//   get state() {
-//     return this._state
-//   },
-//   set state(newState) {
+class TodoStateChanger {
+  constructor (private newState: TodoState) {}
 
-//     if (newState === TodoState.Compelete) 
-//     {
-//       var canBeCompele  =
-//       this.state == TodoState.Active || this.state==TodoState.Delete
-//       if(!canBeCompele) 
-//       {
-//         throw "Todo must be active"
-//       }
-//     }
-//     this._state = newState
-//   }
-// }
-
-// todo.state=TodoState.New;
-
-// console.log(todo.state);
-
-
-class smartTodo {
-  name: string;
-  _state: TodoState;
-  constructor(name:string) {
-    this.name = name;
+  canChangeState(todo:Todo):boolean {
+    return !!todo;
   }
-
-  get state() {
-    return this._state
-  }
-
-  set state(newState) {
-
-    if (newState === TodoState.Compelete) 
+  changeState(todo: Todo): Todo {
+    if(this.canChangeState(todo))
     {
-      var canBeCompele  =
-      this.state == TodoState.Active || this.state==TodoState.Delete
-      if(!canBeCompele) 
-      {
-        throw "Todo must be active"
-      }
+      todo.state=this.newState;
     }
-    this._state = newState
+    return todo;
   }
 }
 
-var todo = new smartTodo("bank");
-todo.state = TodoState.Compelete;
-var st = todo.state;
+
+class compeleteTodoStateChanger extends TodoStateChanger {
+  constructor() {
+    super(TodoState.Compelete)
+  }
+
+  canChangeState(todo: Todo): boolean {
+    return super.canChangeState(todo)&& (todo.state == TodoState.Active || todo.state == TodoState.Deleted)    
+  }
+}
+
+var st = new compeleteTodoStateChanger();
+
